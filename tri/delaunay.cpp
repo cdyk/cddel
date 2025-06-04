33270cecb88ad9269489475d34eea73fa6152f22
Christopher Dyken <cdyk@users.noreply.github.com>
2025-06-04 15:25:37 +0200

    Do edge swapping

--- tri/delaunay.cpp
+++ tri/delaunay.cpp
@@ -2,6 +2,7 @@
 #include <cassert>
 #include <cstdlib>
 #include <cstdio>
+#include <vector>
 #include "delaunay.h"
 
 namespace {
@@ -31,6 +32,24 @@ namespace {
 
     fprintf(stderr, "Failed to allocate memory.");
     exit(EXIT_FAILURE);
+  }
+
+  VtxIx& vertex(const Triangulation& T, HeIx he)
+  {
+    assert(he != NoIx);
+    return T.he[he].vtx;
+  }
+
+  HeIx& next(const Triangulation& T, HeIx he)
+  {
+    assert(he != NoIx);
+    return T.he[he].nxt;
+  }
+
+  HeIx& twin(const Triangulation& T, HeIx he)
+  {
+    assert(he != NoIx);
+    return T.he[he].twin;
   }
 
   uint32_t allocSize(uint32_t minimum, uint32_t allocated)
@@ -90,6 +109,76 @@ namespace {
 
     if (a < b) return -1;
     if (b < a) return 1;
+    return 0;
+  }
+
+  int isDelaunay(const Pos& p1, const Pos& p2, const Pos& p3, const Pos& p4)
+  {
+    // Assuming the quadrilateral is split with the diagonal [p1,p3].
+    // If pi < angle_123 + angle_341, the diagonal should be swapped.
+    //
+    // Since angle_123 + angle_341 < 2pi:
+    //
+    // pi < angle_123 + angle_341 <=>  sin_123 cos_341 + cos_123 sin_341 < 0
+
+    uint16_t x1y2 = p1.x * p2.y;
+    uint16_t x1y3 = p1.x * p3.y;
+    uint16_t x1y4 = p1.x * p4.y;
+
+    uint16_t x2y3 = p2.x * p3.y;
+    uint16_t x2y1 = p2.x * p1.y;
+
+    uint16_t x3y1 = p3.x * p1.y;
+    uint16_t x3y2 = p3.x * p2.y;
+    uint16_t x3y4 = p3.x * p4.y;
+
+    uint16_t x4y1 = p4.x * p1.y;
+    uint16_t x4y3 = p4.x * p3.y;
+
+    // sin_123 = (x3 - x2) (y1 - y2) - (x1 - x2) (y3 - y2)
+    //         = (x3y1 + x1y2 + x2y3) - (x2y1 + x3y2 + x1y3)
+    int32_t sin_123 = (int32_t(x3y1) + int32_t(x1y2) + int32_t(x2y3))
+                    - (int32_t(x2y1) + int32_t(x3y2) + int32_t(x1y3));
+
+    // sin_341 = (x1 - x4) (y3 - y4) - (x3 - x4) (y1 - y4)
+    //         = (x4y1 + x1y3 + x3y4) - (x4y3 + x1y4 + x3y1)
+    int32_t sin_341 = (int32_t(x4y1) + int32_t(x1y3) + int32_t(x3y4))
+                    - (int32_t(x4y3) + int32_t(x1y4) + int32_t(x3y1));
+
+    uint16_t x1x2 = p1.x * p2.x;
+    uint16_t x1x3 = p1.x * p3.x;
+    uint16_t x1x4 = p1.x * p4.x;
+    uint16_t x2x2 = p2.x * p2.x;
+    uint16_t x2x3 = p2.x * p3.x;
+    uint16_t x3x4 = p3.x * p4.x;
+    uint16_t x4x4 = p4.x * p4.x;
+
+    uint16_t y1y2 = p1.y * p2.y;
+    uint16_t y1y3 = p1.y * p3.y;
+    uint16_t y1y4 = p1.y * p4.y;
+    uint16_t y2y2 = p2.y * p2.y;
+    uint16_t y2y3 = p2.y * p3.y;
+    uint16_t y3y4 = p3.y * p4.y;
+    uint16_t y4y4 = p4.y * p4.y;
+
+    // cos_123 = (x3 - x2) (x1 - x2) + (y3 - y2) (y1 - y2)
+    //         = (x2x2 + x1x3 + y2y2 + y1y3) - (y2y3 + x1x2 + x2x3 + y1y2)
+
+    int32_t cos_123 = (x2x2 + x1x3 + y2y2 + y1y3) - (y2y3 + x1x2 + x2x3 + y1y2);
+
+
+    //
+    // cos_341 = (x1 - x4) (x3 - x4) + (y1 - y4) (y3 - y4)]
+    //         = (x1x3  + x4x4 + y1y3 + y4^2) - y1y4 - y3y4  - x1 x4 - x3 x4
+    int32_t cos_341 = (int32_t(x1x3)  + int32_t(x4x4) + int32_t(y1y3) + int32_t(y4y4))
+                    - (int32_t(y1y4) + int32_t(y3y4) + int32_t(x1x4) + int32_t(x3x4));
+
+
+    int64_t test = sin_123 * cos_341 + cos_123 * sin_341;
+
+    if (0 < test) return 1;
+    if (test < 0) return -1;
+
     return 0;
   }
 
@@ -191,6 +280,55 @@ namespace {
                     he3 + 5, he3 + 1, mid);
   }
 
+  void recursiveDelaunaySwap(Triangulation& T, std::vector<HeIx>& todo)
+  {
+    while (!todo.empty()) {
+      HeIx he = todo.back();
+      todo.pop_back();
+      if (he == NoIx) continue;
+
+      HeIx tw = twin(T, he);
+      if (tw == NoIx) continue;
+
+
+      HeIx l0 = next(T, he);
+      HeIx l1 = next(T, l0);
+      HeIx l2 = next(T, tw);
+      HeIx l3 = next(T, l2);
+
+      
+      VtxIx v0 = vertex(T, l0);
+      VtxIx v1 = vertex(T, l1);
+      VtxIx v2 = vertex(T, l2);
+      VtxIx v3 = vertex(T, l3);
+      
+      int del = isDelaunay(T.vtx[v0].pos, T.vtx[v1].pos, T.vtx[v2].pos, T.vtx[v3].pos);
+      if (0 <= del) continue;
+
+      HeIx t0 = twin(T, l0);
+      HeIx t1 = twin(T, l1);
+      HeIx t2 = twin(T, l2);
+      HeIx t3 = twin(T, l3);
+
+      disconnectTriangle(T, he);
+      disconnectTriangle(T, tw);
+
+      connectTriangle(T,
+                      l0, t0, v0,
+                      he, NoIx, v1,
+                      l3, t3, v3);
+
+      connectTriangle(T,
+                      l2, t2, v2,
+                      tw, he, v3,
+                      l1, t1, v1);
+
+      todo.push_back(t0);
+      todo.push_back(t1);
+      todo.push_back(t2);
+      todo.push_back(t3);
+    }
+  }
 
 }
 
@@ -241,6 +379,17 @@ VtxIx insertVertex(Triangulation& triang, const Pos& pos)
   VtxIx v = allocVtx(triang);
   triang.vtx[v].pos = pos;
 
+  std::vector<HeIx> todo;
+  {
+    HeIx e = he;
+    for (int i = 0; i < 3; i++) {
+      todo.push_back(twin(triang, e));
+      e = next(triang, e);
+    }
+  }
   splitTriangle(triang, he, v);
+
+  recursiveDelaunaySwap(triang, todo);
+
   return v;
 }
